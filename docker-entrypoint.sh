a770a108ff31b81f87fb39ff4be4b629308f6df7
Leon Mydla <leon@mydla.dev>
2022-07-11 03:43:10 +0200

    fix permission issues for bind mounted key related files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,10 +7,16 @@ if [[ -e '/mode_server' ]]; then
   cd /run/sshd
 
   if [[ -z "$@" ]] || [[ "$1" = 'sshd' ]]; then
+    cp /authorized_keys /home/client/.ssh/authorized_keys
+    chown client:nogroup /home/client/.ssh/authorized_keys
+    chmod 600 /home/client/.ssh/authorized_keys
     gosu root /usr/sbin/sshd -D -e ${@:2}
   fi
 
   if [[ "$1" = '/usr/sbin/sshd' ]]; then
+    cp /authorized_keys /home/client/.ssh/authorized_keys
+    chown client:nogroup /home/client/.ssh/authorized_keys
+    chmod 600 /home/client/.ssh/authorized_keys
     gosu root $@
   fi
 fi
@@ -18,12 +24,11 @@ fi
 if [[ -e '/mode_client' ]]; then
   cd ~client
 
-  if [[ -z "$@" ]] || [[ "$1" = 'ssh' ]]; then
+  if [[ -z "$@" ]] || [[ "$1" = 'ssh' ]] || [[ "$1" = '/usr/bin/ssh' ]]; then
+    cp /id /home/client/.ssh/id
+    chown client:nogroup /home/client/.ssh/id
+    chmod 600 /home/client/.ssh/id
     gosu client /usr/bin/ssh ${@:2}
-  fi
-
-  if [[ "$1" = '/usr/bin/ssh' ]]; then
-    gosu client $@
   fi
 fi
 
